a3b487f0de936e6647d076eafd06dcbf92b6c66a
teuku muhammad fauzan <teuku.muhammad4279@sma.belajar.id>
2024-12-05 08:11:14 -0800

    my first

---
+++ project1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>                               // project perhitungan yang komplex by teuku muhammad fauzan
+using namespace std;
+void for_pluss(int x1, int y1){
+	int result1 = x1 + y1;
+	cout<< result1;
+}
+void for_kuarang(int x4, int y4){
+	int result2 = x4 - y4;
+	cout<< result2;
+}
+void for_kali(int x2, int y2){
+	int result3 = x2 * y2;
+	cout<< result3;
+}
+void for_bagi(int x3, int y3){
+	int result4 = x3 / y3;
+	cout<< result4;
+}
+int main(){
+	bool Y = true;
+	bool N = false;
+	bool R;
+		cout<<" apakah kamu mau melakukan operasi bilangan 1/0: ";
+		cin>> R;
+	
+		
+}
